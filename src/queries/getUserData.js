c54b1d9573d3410c33982380176969ea052dc2dc
NaremanPro <violette1988@gmail.com>
2019-05-19 11:30:19 +0300

    update the get and post functions

--- src/queries/getUserData.js
+++ src/queries/getUserData.js
@@ -2,23 +2,48 @@
 const dbConnection = require('../database/db_connection.js');
 
 const getUserData = (cb) => {
+  // dbConnection.query('SELECT * FROM students;', (err, res) => {
+  //   console.log('this is the response from students: ', res);
+  //   if(err) return cb(err);
+  //   console.log('res.rows: ', res.rows);
+  //   cb(null, res.rows);
+  // });
+  // dbConnection.query('SELECT * FROM campuses;', (err, res) => {
+  //   console.log('this is the response from campuses: ', res);
+  //   if(err) return cb(err);
+  //   console.log('res.rows: ', res.rows);
+  //   cb(null, res.rows);
+  // });
+  // dbConnection.query('SELECT * FROM rating;', (err, res) => {
+  //   console.log('this is the response from rating: ', res);
+  //   if(err) return cb(err);
+  //   console.log('res.rows: ', res.rows);
+  //   cb(null, res.rows);
+  // });
+
   dbConnection.query('SELECT * FROM students;', (err, res) => {
+    let resArray = [];
     console.log('this is the response from students: ', res);
     if(err) return cb(err);
-    console.log('res.rows: ', res.rows);
-    cb(null, res.rows);
-  });
-  dbConnection.query('SELECT * FROM campuses;', (err, res) => {
-    console.log('this is the response from campuses: ', res);
-    if(err) return cb(err);
-    console.log('res.rows: ', res.rows);
-    cb(null, res.rows);
-  });
-  dbConnection.query('SELECT * FROM rating;', (err, res) => {
-    console.log('this is the response from rating: ', res);
-    if(err) return cb(err);
-    console.log('res.rows: ', res.rows);
-    cb(null, res.rows);
+    console.log('res.rows for students: ', res.rows);
+    resArray += JSON.stringify(res.rows);
+    console.log('resArray is : ', resArray);
+    //save the response somewhere and run the next query
+    dbConnection.query('SELECT * FROM campuses;', (err, res) => {
+        console.log('this is the response from campuses: ', res);
+        if(err) return cb(err);
+        console.log('res.rows for campuses: ', res.rows);
+        resArray += JSON.stringify(res.rows);
+        console.log('resArray after campuses is : ', resArray);
+        dbConnection.query('SELECT * FROM rating;', (err, res) => {
+            console.log('this is the response from rating: ', res);
+            if(err) return cb(err);
+            console.log('res.rows for rating: ', res.rows);
+            resArray += JSON.stringify(res.rows);
+            console.log('resArray after rating is : ', resArray);
+            cb(null, resArray);
+          });
+    });
   });
 };
 
